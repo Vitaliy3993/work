acaf3921de01c905045a0c0e9e0bae65a5a51d0b
vitaliy3993 <kadelchukkp@gmail.com>
2022-06-24 18:13:32 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = () => {
+        const hamburger = document.querySelector('.humburger'),
+              menu = document.querySelector('.mobilemenu'),
+              overlay = document.querySelector('.mobilemenu__overlay');
+    
+    
+        hamburger.addEventListener('click', () => {
+            menu.classList.add('active');
+            let scroll = calcScroll();
+            document.body.style.marginRight = `${scroll}px`;
+        });
+    
+        function closeMenu() {
+            overlay.addEventListener('click', () => {
+                menu.classList.remove('active');
+            });
+        }
+    
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape" && menu.classList.contains('active')) {
+                menu.classList.remove('active');
+            }
+        });
+    
+        function calcScroll() {
+            let div = document.createElement('div');
+    
+            div.style.width = '50px';
+            div.style.height = '50px';
+            div.style.overflowY = 'scroll';
+            div.style.visibility = 'hidden';
+    
+            document.body.appendChild(div);
+            let scrollWidth = div.offsetWidth - div.clientWidth;
+            div.remove();
+    
+            return scrollWidth;
+        }
+    
+        closeMenu();
+    }
+
+    function closeInfo() {
+        const infoSelector = document.querySelector('.info'),
+              closeSelector = document.querySelector('.info__close');
+
+        closeSelector.addEventListener('click', () => {
+            infoSelector.classList.add('hide');
+        });
+    }
+    
+    menu();
+    closeInfo()
+});
+
